1fee55958938a0433845507e9c5195cfdbda4c2e
Vladimír Nevyhoštěný <vnevyhosteny@gmail.com>
2022-10-06 14:42:31 +0200

    feat: process incoming message

--- src/TyntecConversationInboxAdapter.ts
+++ src/TyntecConversationInboxAdapter.ts
@@ -1,12 +1,20 @@
 import {
   Activity,
+  ActivityTypes,
   BotAdapter,
   ConversationReference,
   ResourceResponse,
   TurnContext,
+  WebRequest,
+  WebResponse,
 } from 'botbuilder'
 import { ApiClient, OpenAPIConfig, SendMessageResponse } from './api'
+import { InboundMessage } from './api/generated/models/InboundMessage'
 import { SendTextMessageBodyType } from './api/generated/models/SendTextMessageBodyType'
+import Ajv from 'ajv'
+import { $InboundMessage } from './api/generated/schemas/$InboundMessage'
+import { inboundMessageToActivity } from './ActivityHelper'
+import { ValidationError } from './errors'
 
 export interface TyntecConversationInboxAdapterConfig
   extends Pick<OpenAPIConfig, 'TOKEN'> {
@@ -72,6 +80,37 @@ export class TyntecConversationInboxAdapter extends BotAdapter {
     return this.runMiddleware(context, logic)
   }
 
+  public async processIncomingMessage(
+    req: WebRequest,
+    res: WebResponse,
+    logic: (context: TurnContext) => Promise<any>,
+  ): Promise<void> {
+    if (!this.validateIncomingMessage(req.body)) {
+      res.status(400)
+      res.send('Invalid incoming message')
+      res.end()
+    }
+    const message = req.body as InboundMessage
+
+    try {
+      const context = new TurnContext(this, inboundMessageToActivity(message))
+      await this.runMiddleware(context, logic)
+
+      res.status(200)
+      res.end()
+    } catch (e) {
+      if (e instanceof ValidationError) {
+        res.status(400)
+        res.send(`${e.name}: ${e.message}`)
+        res.end()
+      }
+
+      res.status(500)
+      res.send(`Failed to process incoming message: ${e}`)
+      res.end()
+    }
+  }
+
   public updateActivity(
     _context: TurnContext,
     _activity: Partial<Activity>,
@@ -89,4 +128,15 @@ export class TyntecConversationInboxAdapter extends BotAdapter {
   protected composeWhatsAppChannelJid = (
     params: Pick<TyntecConversationInboxAdapterConfig, 'wabaNumber'>,
   ) => `${params.wabaNumber}@whatsapp.eazy.im`
+
+  private validateIncomingMessage(incoming?: any): incoming is InboundMessage {
+    if (!incoming) {
+      return false
+    }
+
+    const validator = new Ajv()
+    const validate = validator.compile($InboundMessage)
+
+    return validate(incoming)
+  }
 }
